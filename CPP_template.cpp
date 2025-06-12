24d65844fdea4be1adaceb343f76e976e40b0afe
Via-crucis <via_crucis@qq.com>
2025-06-12 21:27:23 +0800

    完成了函数模板的学习

--- CPP_template.cpp
+++ CPP_template.cpp
@@ -12,6 +12,11 @@ class Stu{
         {
             return a.a>b.a;
         }
+        friend ostream& operator<<(ostream& o,Stu a)
+        {
+            o<<a.a<<a.b;        //实际是修改了流运算符左侧变量的值。
+            return o;
+        }
 };
 
 template <typename T>
@@ -34,7 +39,11 @@ class human1:public human<T1>
     T2 bb;
 };
 
-
+template<typename T>
+void print(T x)
+{
+    cout<<x<<endl;
+}
 
 int main()
 {
@@ -49,11 +58,13 @@ int main()
     human1<Stu<int>,int> aaa;   //在类型赋值中添加多个模板类型变量，同时使用类模板继承类模板。
     aaa.aa = 1;
     aaa.bb = 2;
-    cout<<aaa.aa<<endl<<aaa.bb<<endl<<aaa.main();   
+    //cout<<aaa.aa<<endl<<aaa.bb<<endl<<aaa.main();
+    print (a);
 }
 
 
 /*总结:
     1.类型模板是用于替换类中未定义的类型的变量，而每一个类模板在创建时都需要对类型变量（typename）赋值
     2.类模板在继承类模板时，类似构造函数一样，需要为父类模板的类型变量赋值。模板声明是专为类型变量赋值的构造函数
-    3.编译器在识别重载运算符时，会将原来的双目运算符以及其两侧的变量替换为函数调用。*/
+    3.编译器在识别重载运算符时，会将原来的双目运算符以及其两侧的变量替换为函数调用。
+    4.了解了模板函数的作用，以及重载流运算符的基本格式，即friend ostream& operator<<(ostream& o,...)*/
