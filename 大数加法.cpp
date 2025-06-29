cff34a4e9ac1042d31542736c6ba81af9e21dd5f
Via-crucis <via_crucis@qq.com>
2025-06-29 18:51:54 +0800

    完成了大数乘法的学习，书写了sal1和sal2的两个函数，sal1在主循环外判断并删除先导0或者进位。sal2在主循环内判断先导0和进位。

--- 大数加法.cpp
+++ 大数加法.cpp
@@ -2,6 +2,43 @@
 #include <vector>
 #include <string>
 using namespace std;
+vector <int> sal1 (vector <int> &a , int b)
+{
+    vector <int> c;
+    int t = 0;
+    int carry = 0;
+    int a_size = a.size();
+    for(int i = 0 ; i < a_size ; i++)
+    {
+        t += a[i] * b;
+        c.push_back(t % 10);
+        t /= 10;
+    }
+    if (t) c.push_back(t);
+    while(c.size() > 1 && c.back() == 0) c.pop_back();  //去除先导0
+    for(int i = c.size() - 1 ; i >= 0 ; i--) cout << c [i];
+    return c;
+
+}
+
+vector <int> sal2 (vector <int> &a , int b)
+{
+    vector <int> c;
+    int t = 0;
+    int carry = 0;
+    int a_size = a.size();
+    for(int i = 0 ; i < a_size || t; i++)
+    {
+        t += a[i] * b;
+        c.push_back(t % 10);
+        t /= 10;
+    }
+    /*if (t) c.push_back(t);
+    while(c.size() > 1 && c.back() == 0) c.pop_back();  //去除先导0*/
+    for(int i = c.size() - 1 ; i >= 0 ; i--) cout << c [i];
+    return c;
+
+}
 bool cmp (vector <int> &a , vector <int> &b)
 {
     if (a.size() != b.size()) return a.size() > b.size();
@@ -32,6 +69,8 @@ vector <int> sub(vector <int> &a , vector <int> &b)
     for(int i = c.size() - 1 ; i >= 0 ; i--) cout << c [i];
     return c;
 }
+
+
 vector <int> add(vector <int> &a , vector <int> &b)
 {
     vector<int> c;
@@ -58,9 +97,10 @@ int main()
 {
     vector<int>a,b;
     string num1,num2;
-    cin >> num1 >> num2;
+    int num3;
+    cin >> num1 >> num3;
     for(int i = num1.size()-1 ; i >= 0 ;i--) a.push_back(num1[i] - '0');
     for(int i = num2.size()-1 ; i >= 0 ;i--) b.push_back(num2[i] - '0');
-    sub (a , b);
+    sal2 (a , num3);
     cout<<endl;
 }
