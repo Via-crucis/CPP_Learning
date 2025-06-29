2ffe8d738460c1a605dad1b96066ef71e8549915
Via-crucis <via_crucis@qq.com>
2025-06-29 10:49:15 +0800

    完成了高精度加法和高精度减法的学习

---
+++ 大数加法.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+bool cmp (vector <int> &a , vector <int> &b)
+{
+    if (a.size() != b.size()) return a.size() > b.size();
+    for (int i = a.size() - 1 ; i >= 0 ; i--)
+    {
+        if (a[i] == b[i]) continue;
+        return a[i] > b[i];
+    }
+    return 1;
+}
+vector <int> sub(vector <int> &a , vector <int> &b)
+{
+    if (!cmp(a , b)) 
+    {
+        cout << "-";
+        return sub (b , a);
+    }
+    vector <int> c;
+    int t = 0;
+    for(int i = 0 ;i < a.size() ;i++)
+    {
+        t = a[i] - t;
+        if(i < b.size()) t -= b[i];
+        c.push_back((t + 10) % 10);
+        t < 0 ? t = 1 : t = 0;
+    }
+    while(c.size() > 1 && c.back() == 0) c.pop_back();  //去除先导0
+    for(int i = c.size() - 1 ; i >= 0 ; i--) cout << c [i];
+    return c;
+}
+vector <int> add(vector <int> &a , vector <int> &b)
+{
+    vector<int> c;
+    int t = 0;
+    int a_size = a.size();
+    int b_size = b.size();
+    for(int i = 0;i < a_size || i < b_size ; i++)
+        {
+            if (i < a_size) t += a[i];
+            if (i < b_size) t += b[i];
+            c.push_back(t % 10);
+            t /= 10;
+        }
+        if (t) c.push_back(t);
+
+
+    
+    while(c.size() > 1 && c.back() == 0) c.pop_back();  //去除先导0
+    for(int i = c.size() - 1 ; i >= 0 ; i--) cout << c [i];
+    return c;
+}
+
+int main()
+{
+    vector<int>a,b;
+    string num1,num2;
+    cin >> num1 >> num2;
+    for(int i = num1.size()-1 ; i >= 0 ;i--) a.push_back(num1[i] - '0');
+    for(int i = num2.size()-1 ; i >= 0 ;i--) b.push_back(num2[i] - '0');
+    sub (a , b);
+    cout<<endl;
+}
